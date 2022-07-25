7db27cd01c412fce086466884e466ea902354611
Nora Vargas <noravgs92@gmail.com>
2022-07-25 14:36:59 -0500

    added port connection & middleware

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
 
 
-
+//mongodb connection
 let db,
 dbConnectionString = process.env.DB_STRING,
 dbName = 'lit',
@@ -16,4 +16,19 @@ MongoClient.connect(dbConnectionString)
  console.log('Connected to Database')
  db = client.db(dbName)
  collection = db.collection('books')
+})
+
+
+//middleware
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(cors())
+
+
+
+//port connection
+app.listen(process.env.PORT || PORT, () => {
+ console.log(`Server is running on port ${process.env.PORT}`)
 })
