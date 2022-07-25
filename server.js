40d0a1af219a6ebf8a9a8bc200d0e5d18d970f25
Nora Vargas <noravgs92@gmail.com>
2022-07-25 13:21:37 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const app = express()
+const cors = require('cors')
+const MongoClient = require('mongodb').MongoClient
+require('dotenv').config()
+
+
+
+let db,
+dbConnectionString = process.env.DB_STRING,
+dbName = 'lit',
+collection  
+
+MongoClient.connect(dbConnectionString)
+.then(client => {
+ console.log('Connected to Database')
+ db = client.db(dbName)
+ collection = db.collection('books')
+})
